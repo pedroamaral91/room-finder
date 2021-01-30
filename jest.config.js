736ef4a470d3d65c2585e5ebd63d561f0d05ad29
pedroamaral91 <pedrovinicius91@gmail.com>
2021-01-30 09:32:53 -0300

    chore: init and config entire project

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  bail: 1,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
