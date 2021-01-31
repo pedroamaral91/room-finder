d1946b1d4850d897ce31169489ff76cdc6c731ac
pedroamaral91 <pedrovinicius91@gmail.com>
2021-01-31 17:51:00 -0300

    feat: create validation layer

---
+++ src/validation/date-validation.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-useless-escape */
+import { Validation } from '@/presentation/protocols/validation'
+import { BAD_REQUEST } from '../presentation/errors/bad-request'
+export class DateValidation implements Validation {
+  private readonly REGEX = /^\d{2}([\/])\d{2}\1\d{4}$/
+  constructor (
+    private readonly fieldName: string
+  ) {}
+
+  validate (input: any): Error {
+    if (!this.REGEX.test(input[this.fieldName])) return new BAD_REQUEST(this.fieldName)
+  }
+}
